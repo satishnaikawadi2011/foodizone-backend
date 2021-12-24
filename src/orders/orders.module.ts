4a2d67a262e145f8277789c2e0ea88e55af391d7
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-12-24 22:29:54 +0530

    Successfully added createOrder mutation which has very heavy and complex business logic code.

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { Order } from './entities/order.entity';
+import { OrderResolver } from './orders.resolver';
+import { OrderService } from './orders.service';
 
-@Module({})
+@Module({
+	imports:
+		[
+			TypeOrmModule.forFeature([
+				Order,
+				OrderItem,
+				Dish,
+				Restaurant
+			])
+		],
+	providers:
+		[
+			OrderService,
+			OrderResolver
+		]
+})
 export class OrdersModule {}
---
+++ src/orders/orders.resolver.ts
@@ -0,0 +1,24 @@
+import { Inject } from '@nestjs/common';
+import { Args, Mutation, Resolver, Query, Subscription } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { Order } from './entities/order.entity';
+import { OrderService } from './orders.service';
+
+@Resolver((of) => Order)
+export class OrderResolver {
+	constructor(private readonly ordersService: OrderService) {}
+
+	@Mutation((returns) => CreateOrderOutput)
+	@Role([
+		'Client'
+	])
+	async createOrder(
+		@AuthUser() customer: User,
+		@Args('input') createOrderInput: CreateOrderInput
+	): Promise<CreateOrderOutput> {
+		return this.ordersService.createOrder(customer, createOrderInput);
+	}
+}
