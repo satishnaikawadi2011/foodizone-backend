de874cf81d37ca6efb5e99574fd5b691041e8458
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-12-25 07:00:55 +0530

    Successfully added createPayment mutation so that Restaurant owners can pay us to promote their restaurants.

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,57 @@
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import { LessThan, Repository } from 'typeorm';
+import { CreatePaymentInput, CreatePaymentOuput } from './dtos/create-payment.dto';
+import { Payment } from './entities/payment.entity';
+
+@Injectable()
+export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly payments: Repository<Payment>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+  ) {}
+
+  async createPayment(
+    owner: User,
+    { transactionId, restaurantId }: CreatePaymentInput,
+  ): Promise<CreatePaymentOuput> {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant not found !!',
+        };
+      }
+      if (restaurant.ownerId !== owner.id) {
+        return {
+          ok: false,
+          error: 'You are not allowed to do this !!',
+        };
+      }
+      await this.payments.save(
+        this.payments.create({
+          transactionId,
+          user: owner,
+          restaurant,
+        }),
+      );
+    //   restaurant.isPromoted = true;
+      const date = new Date();
+      date.setDate(date.getDate() + 7);
+    //   restaurant.promotedUntil = date;
+      this.restaurants.save(restaurant);
+      return {
+        ok: true,
+      };
+    } catch {
+      return { ok: false, error: 'Failed to complete the payment !!' };
+    }
+  }
+
+}
